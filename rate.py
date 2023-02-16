81aee756b2fa78aac98b7b71f094ac961790924f
Titus Muthomi <muthomititus+github@gmail.com>
2023-02-16 22:15:21 +0300

    Convert celcius to farenheits

---
+++ rate.py
@@ -0,0 +1,6 @@
+hours = input("Enter number of hours: ")
+rate = input("Enter  rate per hour: ")
+
+pay = float(hours) * float(rate)
+
+print(f"your pay is {pay}")
---
+++ temperature.py
@@ -0,0 +1,5 @@
+celcius_temperature = float(input("enter temperature: "))
+
+fahrenheit_temperature = celcius_temperature * 1.8 + 32
+
+print(f"{fahrenheit_temperature} farenheits")
