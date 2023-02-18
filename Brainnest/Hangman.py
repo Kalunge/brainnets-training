26617db412f06bca7b78250f23a194d57d0059c7
Titus Muthomi <muthomititus+github@gmail.com>
2023-02-18 14:28:56 +0300

    Add black python formatter

---
+++ Brainnest/Hangman.py
@@ -0,0 +1,23 @@
+'''
+The hangman game is a word guessing game where the player is given a word and has to guess the letters that make up the word. 
+The player is given a certain number of tries (no more than 6 wrong guesses are allowed) to guess the correct letters before the game is over.
+'''
+
+# Output
+'''
+You have 6 tries left.                                                                                                                                           
+Used letters:                                                                                                                                                    
+Word: _ _ _ _                                                                                                                                                    
+Guess a letter: a 
+
+You have 6 tries left.                                                                                                                                           
+Used letters: a                                                                                                                                                  
+Word: _ a _ a                                                                                                                                                    
+Guess a letter: j    
+
+You have 6 tries left.                                                                                                                                           
+Used letters: j a                                                                                                                                                
+Word: j a _ a                                                                                                                                                    
+Guess a letter: v                                                                                                                                                
+You guessed the word java !
+'''
---
+++ Brainnest/Weather App.py
@@ -0,0 +1,14 @@
+''' The goal of this project is to create a weather app that shows the current weather conditions and forecast for a specific location.
+
+Here are the steps you can take to create this project:
+
+    Use the requests library to make an API call to a weather service (e.g. OpenWeatherMap) to retrieve the weather data for a specific location.
+
+    Use the json library to parse the JSON data returned by the API call.
+
+    Use the tkinter library to create a GUI for the app, including widgets such as labels, buttons and text boxes.
+
+    Use the Pillow library to display the weather icons.
+
+    Use the datetime library to display the current time and date. '''
+
