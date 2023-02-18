946831355091206d077219a533556e64361e2fc3
Titus Muthomi <muthomititus+github@gmail.com>
2023-02-18 14:23:45 +0300

    Add solution to score test

---
+++ score.py
@@ -0,0 +1,19 @@
+try:
+  score = float(input("Enter score between 0.0 and 1.0: "))
+  if score > 1.0 or score < 0.0:
+    print("score is out of range")
+  elif score >= 0.9:
+    print("A")
+  elif score >= 0.8:
+    print("B")
+  elif score >= 0.7:
+    print("C")
+  elif score >= 0.6:
+    print("D")
+  else:
+    print("E")
+except ValueError:
+  print("Enter a valid integer or float")
+
+
+
