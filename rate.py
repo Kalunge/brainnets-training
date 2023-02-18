5f1b0a265fe540a33af73cc245948b3291dc4d91
Titus Muthomi <muthomititus+github@gmail.com>
2023-02-18 14:28:24 +0300

    update rate to use error handler

--- rate.py
+++ rate.py
@@ -1,6 +1,27 @@
-hours = input("Enter number of hours: ")
-rate = input("Enter  rate per hour: ")
+hours = float(input("Enter  number of hours: "))
+rate = float(input("Enter  rate per hour: "))
 
-pay = float(hours) * float(rate)
+pay = 0
+
+
+
+if hours > 40:
+  try:
+    pay = (40 * rate) + (hours - 40) *(rate * 1.5)
+  except:
+    print("An exception occurred")
+else:
+  pay = hours * rate
+
+
+
 
 print(f"your pay is {pay}")
+
+# hours = 41
+# rate = 2
+
+# pay = (40 * 2) + (1 *(1 * 15))
+#  total = 80 + 1.5 = 81.5
+
+
