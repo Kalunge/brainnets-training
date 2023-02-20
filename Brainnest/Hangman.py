23bdbda7e3f271fa346e09315e27bae1be7254bb
Titus Muthomi <muthomititus+github@gmail.com>
2023-02-20 11:50:45 +0300

    Add hangman solution

--- Brainnest/Hangman.py
+++ Brainnest/Hangman.py
@@ -21,3 +21,41 @@ Word: j a _ a
 Guess a letter: v                                                                                                                                                
 You guessed the word java !
 '''
+
+# Code
+import random
+
+def hangman():
+    word = random.choice(["python", "java", "kotlin", "javascript"])
+    word_list = list(word)
+    word_list_copy = list(word)
+    word_list_copy = ["-" for i in range(len(word_list_copy))]
+    tries = 6
+    used_letters = []
+    while tries > 0:
+        print(f"You have {tries} tries left.")
+        print("Used letters:", " ".join(used_letters))
+        print("Word:", " ".join(word_list_copy))
+        guess = input("Guess a letter: ")
+        if len(guess) != 1:
+            print("You should input a single letter")
+        elif guess.islower() == False:
+            print("It is not an ASCII lowercase letter")
+        elif guess in used_letters:
+            print("You already typed this letter")
+        elif guess in word_list:
+            for i in range(len(word_list)):
+                if word_list[i] == guess:
+                    word_list_copy[i] = guess
+            used_letters.append(guess)
+        else:
+            print("No such letter in the word")
+            used_letters.append(guess)
+            tries -= 1
+        if "-" not in word_list_copy:
+            print("You guessed the word {} !".format(word))
+            break
+    else:
+        print("You are hanged!")
+
+hangman()
