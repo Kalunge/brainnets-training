f43c8331fb963a6a7ee23228dc40eb79c30c09b0
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-01 21:30:47 +0300

    Organize wuiz and projects in folders

---
+++ file/open.py
@@ -0,0 +1,26 @@
+# open test.text if exists
+
+import os
+
+def open_file():
+    if os.path.exists("test.txt"):
+        f = open("test.txt")
+        print(f.read())
+    else:
+        print("File not found")
+
+open_file()
+
+# find patter in file mbox-short.txt and print the number of lines that match the pattern
+
+# import re
+
+# def find_pattern():
+#     count = 0
+#     with open("mbox-short.txt") as f:
+#         for line in f:
+#             if re.search("^From", line):
+#                 count += 1
+#     print(count)
+
+# find_pattern()
