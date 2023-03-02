924a033c61cd09101956e343076f04a570d308eb
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-02 22:28:38 +0300

    Add authorization

--- decorators/index.py
+++ decorators/index.py
@@ -8,6 +8,20 @@
 # and by calling say_hello() at the end, also the greet(func) should be
 # provoked.
 
+def greet(func):
+    def wrapper(*args):
+        print("Hello")
+        func(*args)
+    return wrapper
+
+@greet
+def say_hello(name):
+    print(f"My name is {name}")
+
+say_hello("John")
+
+
+
  
 
 #  2.
@@ -20,6 +34,25 @@
 # By calling secret_data you should see if the data is confidential or 
 # you will provoke the other function that says "Unauthorized access".
 
+def authorize(func):
+    def wrapper(*args):
+        if not is_authorized:
+            return "Unauthorized access"
+        return func(*args)
+    return wrapper
+
+def check_auth():
+    return True
+
+@authorize
+def secret_data():
+    print("This is confidential data")
+
+is_authorized = check_auth()
+secret_data()
+
+
+
 
 # 3.
 # write a function called validate(func), define a wrapper inside,
@@ -28,3 +61,20 @@
 # when calling the func add() in the end, if the args are integers return 
 # the sum, if even one of them in str, return that error you defined in the
 # first function.
+
+def validate(func):
+    def wrapper(*args):
+        for arg in args:
+            if type(arg) != int:
+                return "Invalid input"
+        return func(*args)
+    return wrapper
+
+@validate
+def add(a, b):
+    return a + b
+
+
+print(add(1, 2))
+print(add(1, "2"))
+
