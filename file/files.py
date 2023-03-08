d132f536c1a0b55266ce6e57ab3d3f58d8a8255a
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-08 19:55:15 +0300

    Add sum numbers solution

--- file/files.py
+++ file/files.py
@@ -33,6 +33,29 @@ my_dict = csv_to_dict(filename)
 
 
     # 3. Write a Python program that reads a binary file and converts it into a hexadecimal string. The program should output the hexadecimal string to a text file.
+
+def bin_to_hex(filename):
+    """Convert a binary file to a hexadecimal string."""
+    with open(filename, 'rb') as f:
+        data = f.read()
+        hex_data = data.hex()
+    return hex_data
+
+filename = 'data.bin'
+hex_data = bin_to_hex(filename)
+
+
+
     # 4. Write a Python program that reads a text file containing numbers and calculates the sum of all the numbers in the file.
+
+def sum_numbers(filename):
+    """Calculate the sum of all the numbers in a file."""
+    with open(filename, 'r') as f:
+        numbers = f.readlines()
+        numbers = [int(x.strip()) for x in numbers]
+        total = sum(numbers)
+    return total
+
+    
     # 5. Write a Python program that reads a text file and removes all the blank lines. The modified text should be written back to the file.
 
