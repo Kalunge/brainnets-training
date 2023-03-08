59591d334aa6f27bc22a628f9407b536cb202234
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-08 19:56:24 +0300

    Add remove blanks solution

--- file/files.py
+++ file/files.py
@@ -14,8 +14,7 @@ def count_words(filename):
         num_words = len(words)
         print("The file " + filename + " has about " + str(num_words) + " words.")
 
-filename = 'alice.txt'
-count_words(filename)
+
 
     # 2. Write a Python program that reads a CSV file and converts it into a dictionary. Each row of the CSV file should be a key-value pair in the dictionary.
 
@@ -28,8 +27,7 @@ def csv_to_dict(filename):
         my_dict = dict(reader)
     return my_dict
 
-filename = 'data.csv'
-my_dict = csv_to_dict(filename)
+
 
 
     # 3. Write a Python program that reads a binary file and converts it into a hexadecimal string. The program should output the hexadecimal string to a text file.
@@ -41,12 +39,10 @@ def bin_to_hex(filename):
         hex_data = data.hex()
     return hex_data
 
-filename = 'data.bin'
-hex_data = bin_to_hex(filename)
 
 
 
-    # 4. Write a Python program that reads a text file containing numbers and calculates the sum of all the numbers in the file.
+# 4. Write a Python program that reads a text file containing numbers and calculates the sum of all the numbers in the file.
 
 def sum_numbers(filename):
     """Calculate the sum of all the numbers in a file."""
@@ -56,6 +52,16 @@ def sum_numbers(filename):
         total = sum(numbers)
     return total
 
-    
-    # 5. Write a Python program that reads a text file and removes all the blank lines. The modified text should be written back to the file.
+
+# 5. Write a Python program that reads a text file and removes all the blank lines. The modified text should be written back to the file.
+
+def remove_blank_lines(filename):
+    """Remove all the blank lines from a file."""
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        lines = [x.strip() for x in lines if x.strip()]
+    return lines
+
+
+
 
