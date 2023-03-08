b6a5e14bfb0201e8c31835ad3a9164b1a4a41ad9
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-08 20:15:08 +0300

    Add transfer_files solution

--- file/Automating File Transfer.py
+++ file/Automating File Transfer.py
@@ -17,5 +17,45 @@ Here are the steps you can take to automate this process:
     You can also set up a log file to keep track of the files that have been transferred and any errors that may have occurred during the transfer process. '''
 
 
+def transfer_files():
+    """Transfer files from external FTP server to internal network."""
+    # Connect to the FTP server and login.
+    ftp = ftplib.FTP('ftp.example.com')
+    ftp.login(user='user', passwd='password')
+
+    # Change to the correct directory and get a list of the files.
+    ftp.cwd('/files')
+    files = ftp.nlst()
+
+    # Create a local directory to store the files.
+    if not os.path.exists('files'):
+        os.makedirs('files')
+
+    # Download each file from the FTP server and save it to the local directory.
+    for file in files:
+        with open(os.path.join('files', file), 'wb') as f:
+            ftp.retrbinary('RETR ' + file, f.write)
+
+    # Disconnect from the FTP server.
+    ftp.quit()
+
+    # Move the files from the local directory to the internal network.
+    for file in files:
+        shutil.move(os.path.join('files', file), '/internal/files')
+
+    # Log the successful transfer.
+    with open('transfer_log.txt', 'a') as f:
+        f.write('Files transferred successfully at {}.\n'.format(datetime.now()))
+
+    # Schedule the script to run daily at 5:00 AM.
+    schedule.every().day.at('05:00').do(transfer_files)
+
+    # Run the script until you close the terminal.
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == '__main__':
+    transfer_files()
 
 
