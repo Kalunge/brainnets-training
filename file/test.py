8369cb07dde6faed5b0970249c3d8ec1cf66b01e
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-08 20:08:55 +0300

    Add sum_amicable_numbers solution

--- file/test.py
+++ file/test.py
@@ -1,13 +1,19 @@
-# 6. 
+# 7. 
 
-# n! means n × (n − 1) × ... × 3 × 2 × 1
+# Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+# If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
 
-# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
-# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+# For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 
-# Find the sum of the digits in the number 100!
+# Evaluate the sum of all the amicable numbers under 10000.
 
-def factorial(n):
-    if n == 0:
-        return 1
-    return n * factorial(n - 1)
+def sum_amicable_numbers(n):
+    """Calculate the sum of all the amicable numbers under n."""
+    amicable_numbers = []
+    for i in range(1, n):
+        if i not in amicable_numbers:
+            sum_divisors = sum_divisors(i)
+            if sum_divisors(i) != i and sum_divisors(sum_divisors(i)) == i:
+                amicable_numbers.append(i)
+                amicable_numbers.append(sum_divisors(i))
+    return sum(amicable_numbers)
