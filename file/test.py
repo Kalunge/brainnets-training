96720ebee4bc3487afa238371ceaabd2dde5482f
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-08 20:10:14 +0300

    Add permute solution

--- file/test.py
+++ file/test.py
@@ -1,14 +1,17 @@
-# 8. 
+# 9. 
 
-# A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+# A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
 
-# A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
+# 012   021   102   120   201   210
 
-# As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+# What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
-# Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
-
-def is_abundant(n):
-    """Check if a number is abundant."""
-    divisors = [i for i in range(1, n) if n % i == 0]
-    return sum(divisors) > n
+def permute(s):
+    out = []
+    if len(s) == 1:
+        out = [s]
+    else:
+        for i, let in enumerate(s):
+            for perm in permute(s[:i] + s[i+1:]):
+                out += [let + perm]
+    return out
