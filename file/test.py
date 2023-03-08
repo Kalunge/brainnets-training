54bd8eb1af40bcd3e698e55e4d03387bbb864eff
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-08 20:08:09 +0300

    Add factorial solution

--- file/test.py
+++ file/test.py
@@ -1,26 +1,13 @@
-# 5. 
+# 6. 
 
-# You are given the following information, but you may prefer to do some research for yourself.
+# n! means n × (n − 1) × ... × 3 × 2 × 1
 
-#     1 Jan 1900 was a Monday.
-#     Thirty days has September,
-#     April, June and November.
-#     All the rest have thirty-one,
-#     Saving February alone,
-#     Which has twenty-eight, rain or shine.
-#     And on leap years, twenty-nine.
-#     A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
-# How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+# Find the sum of the digits in the number 100!
 
-def is_leap_year(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
