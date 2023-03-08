563360e8cdddf4d1d7bdc0d4e692d5e2ddd94796
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-08 20:05:02 +0300

    Add count_letters solution

--- file/test.py
+++ file/test.py
@@ -1,9 +1,20 @@
-# 3. 
+# 4. 
 
-# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+# If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
 
-# What is the sum of the digits of the number 2^1000?
+# If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
 
-def sum_digits(n):
-    """Sum the digits of a number."""
-    return sum(int(i) for i in str(n))
+# NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+
+
+def count_letters(n):
+    """Count the number of letters in a number written out in words."""
+    if n == 0:
+        return 0
+    if n < 20:
+        return len(ones[n])
+    if n < 100:
+        return len(tens[n // 10]) + count_letters(n % 10)
+    if n < 1000:
+        return len(ones[n // 100]) + len('hundred') + (3 if n % 100 != 0 else 0) + count_letters(n % 100)
+    return len('onethousand')
