78bc853616ff9916f1394925b96c75e86a25ec9a
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-08 20:04:10 +0300

    Add sum_digits solution

--- file/test.py
+++ file/test.py
@@ -1,21 +1,9 @@
-# 2. 
+# 3. 
 
-# The following iterative sequence is defined for the set of positive integers:
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
-# n → n/2 (n is even)
-# n → 3n + 1 (n is odd)
+# What is the sum of the digits of the number 2^1000?
 
-# Using the rule above and starting with 13, we generate the following sequence:
-# 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
-
-# It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
-
-# Which starting number, under one million, produces the longest chain?
-
-# NOTE: Once the chain starts the terms are allowed to go above one million.
-
-def longest_chain(n):
-    if n % 2 == 0:
-        return n / 2
-    else:
-        return 3 * n + 1
+def sum_digits(n):
+    """Sum the digits of a number."""
+    return sum(int(i) for i in str(n))
