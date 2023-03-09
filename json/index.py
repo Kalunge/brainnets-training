2f0a4983869a7f87947ff5587bffe42c25db4577
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-09 23:01:40 +0300

    Add users followers solution

--- json/index.py
+++ json/index.py
@@ -22,3 +22,27 @@ with open('products.json') as f:
             max_price = product['price']
             max_product = product
     print(max_product['name'], max_product['price'])
+
+# {1: [2, 3], 2: [1], 3: [1]} from users.json
+
+# Write a Python program that reads in a JSON file containing data on a set of users,
+
+# and outputs a dictionary containing the number of friends that each user has.
+
+
+with open('users.json') as f:
+    data = json.load(f)
+    friends = {}
+    for user in data:
+        friends[user['id']] = len(user['friends'])
+    print(friends)
+
+    # Write a Python program that takes a JSON file containing data on a set of users, and creates a dictionary where each key is a user ID and each value is a list of the user's followers.
+
+
+with open('users.json') as f:
+    data = json.load(f)
+    followers = {}
+    for user in data:
+        followers[user['id']] = user['followers']
+    print(followers)
