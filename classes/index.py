ee65d0e74ce110267505906561aa3e06afa6c5e2
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-15 21:59:20 +0300

    Add Pandigital solution

--- classes/index.py
+++ classes/index.py
@@ -293,3 +293,36 @@ def coin_sums():
             ways[i] += ways[i - coin]
 
     return ways[200]
+print(coin_sums())
+
+# We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
+
+# The product 7254 is unusual, as the identity, 39 × 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
+
+# Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
+# HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
+# use class 
+
+class Pandigital:
+    def __init__(self):
+        self.products = set()
+
+    def is_pandigital(self, n):
+        digits = set()
+        while n > 0:
+            digits.add(n % 10)
+            n //= 10
+        return len(digits) == 9 and 0 not in digits
+
+    def check(self, a, b):
+        c = a * b
+        if self.is_pandigital(a) and self.is_pandigital(b) and self.is_pandigital(c):
+            self.products.add(c)
+
+    def solve(self):
+        for i in range(1, 10000):
+            for j in range(i, 10000):
+                self.check(i, j)
+        return sum(self.products)
+
+print(Pandigital().solve())
