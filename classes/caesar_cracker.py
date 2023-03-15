14217f586697114b0d72f19d75ed89309c0e8a66
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-15 21:50:05 +0300

    Add Rectangle solution

---
+++ classes/caesar_cracker.py
@@ -0,0 +1,7 @@
+'''
+This program can hack messages encrypted 
+with the Caesar cipher from the previous project, even 
+if you don’t know the key. There are only 26 
+possible keys for the Caesar cipher, so a computer can easily try all possible decryptions and display the results to the user. In cryptography, we call 
+this technique a brute-force attack.
+'''
